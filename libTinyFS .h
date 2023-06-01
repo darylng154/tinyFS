8b26a94b359546bc6bc904d3c0bd37a55056f593
Daryl Ng <daryl.ng154@gmail.com>
2023-06-01 14:08:19 -0700

    created header file

---
+++ libTinyFS .h
@@ -0,0 +1,26 @@
+#ifndef LIBTINYFS_H
+#define LIBTINYFS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#define BLOCKSIZE 256
+#define DEFAULT_DISK_SIZE 10240 
+#define DEFAULT_DISK_NAME “tinyFSDisk” 	
+typedef int fileDescriptor;
+
+
+int tfs_mkfs(char *filename, int nBytes);
+int tfs_mount(char *filename);
+int tfs_unmount(void);
+fileDescriptor tfs_open(char *name);
+int tfs_close(fileDescriptor FD);
+int tfs_write(fileDescriptor FD, char *buffer, int size);
+int tfs_delete(fileDescriptor FD);
+int tfs_readByte(fileDescriptor FD, char *buffer);
+int tfs_seek(fileDescriptor FD, int offset);
+
+
+#endif
